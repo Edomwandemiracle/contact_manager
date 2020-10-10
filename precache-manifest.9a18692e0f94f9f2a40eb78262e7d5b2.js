1579ba14e335742854deefdae8496eb48fb3c239
Edomwandemiracle <miracleedomwande@gmail.com>
2020-10-10 17:18:15 +0100

    Updates

---
+++ precache-manifest.9a18692e0f94f9f2a40eb78262e7d5b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f9d6acb28b53a86910a54be6434cf8",
+    "url": "/contact_manager/index.html"
+  },
+  {
+    "revision": "62dd19204baa7162d2de",
+    "url": "/contact_manager/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f2f16779cd1088f85e0c",
+    "url": "/contact_manager/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "62dd19204baa7162d2de",
+    "url": "/contact_manager/static/js/2.a1ec74f1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contact_manager/static/js/2.a1ec74f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f16779cd1088f85e0c",
+    "url": "/contact_manager/static/js/main.21acdf2c.chunk.js"
+  },
+  {
+    "revision": "ca9edcc0c0151d10c1a3",
+    "url": "/contact_manager/static/js/runtime-main.f99e3ccd.js"
+  }
+]);
